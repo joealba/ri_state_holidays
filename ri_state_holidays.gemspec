4440faedcf161fb890b03322f85ec14cf90aefb1
Joe Alba <joe@joealba.com>
2015-05-20 13:15:08 -0400

    Updated specs for rspec >= 3

--- ri_state_holidays.gemspec
+++ ri_state_holidays.gemspec
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 Gem::Specification.new do |gem|
-  gem.authors       = ["Joseph Alba"]
-  gem.email         = ["jalba@egov.com"]
+  gem.authors       = ["Joe Alba"]
+  gem.email         = ["joe@joealba.com"]
   gem.description   = %q{Calculate Rhode Island state holidays}
   gem.summary       = %q{Based off the Holidays Ruby gem -- but without altering the Date class}
   gem.homepage      = ""
@@ -13,8 +13,8 @@ Gem::Specification.new do |gem|
   gem.name          = "ri_state_holidays"
   gem.require_paths = ["lib"]
 
-  gem.version       = '0.1.0'
+  gem.version       = '0.1.1'
 
-  gem.add_development_dependency "rspec"
+  gem.add_development_dependency "rspec", ">= 3.0.0"
   gem.add_development_dependency "rake"
 end
--- spec/ri_state_holidays_spec.rb
+++ spec/ri_state_holidays_spec.rb
@@ -4,19 +4,19 @@ describe RiStateHolidays do
 
   describe "easy day-of-month holidays" do
     it "knows New Years Day" do
-      RiStateHolidays.holiday?(Date.new(2013, 1, 1)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2013, 1, 1))).to eq(true)
     end
 
     it "knows Independence Day" do
-      RiStateHolidays.holiday?(Date.new(2012, 7, 4)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 7, 4))).to eq(true)
     end
 
     it "knows Christmas" do
-      RiStateHolidays.holiday?(Date.new(2012, 12, 25)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 12, 25))).to eq(true)
     end
 
     it "knows a non-holiday" do
-      RiStateHolidays.holiday?(Date.new(2012, 2, 28)).should be_false
+      expect(RiStateHolidays.holiday?(Date.new(2012, 2, 28))).to eq(false)
     end
   end
 
@@ -24,43 +24,43 @@ describe RiStateHolidays do
   describe "observed holidays" do
     # When any legal holiday falls on a Sunday, the day following it is a full holiday.
     it "observed New Years from a Sunday" do
-      RiStateHolidays.holiday?(Date.new(2012, 1, 2)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 1, 2))).to eq(true)
     end
 
     it "observed Veterans Day from a Sunday" do
-      RiStateHolidays.holiday?(Date.new(2012, 11, 12)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 11, 12))).to eq(true)
     end
   end
 
 
   describe "calculated holidays" do
     it "knows Veteran's Day" do
-      RiStateHolidays.holiday?(Date.new(2013, 11, 11)).should be_true
-      RiStateHolidays.holiday?(Date.new(2014, 11, 11)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2013, 11, 11))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2014, 11, 11))).to eq(true)
     end
 
     it "knows Victory Day" do
-      RiStateHolidays.holiday?(Date.new(2012, 8, 13)).should be_true
-      RiStateHolidays.holiday?(Date.new(2013, 8, 12)).should be_true
-      RiStateHolidays.holiday?(Date.new(2014, 8, 11)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 8, 13))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2013, 8, 12))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2014, 8, 11))).to eq(true)
     end
 
     it "knows Labor Day" do
-      RiStateHolidays.holiday?(Date.new(2012, 9, 3)).should be_true
-      RiStateHolidays.holiday?(Date.new(2013, 9, 2)).should be_true
-      RiStateHolidays.holiday?(Date.new(2014, 9, 1)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 9, 3))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2013, 9, 2))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2014, 9, 1))).to eq(true)
     end
 
     it "knows election day" do
-      RiStateHolidays.holiday?(Date.new(2012, 11, 6)).should be_true
-      RiStateHolidays.holiday?(Date.new(2013, 11, 5)).should be_false
-      RiStateHolidays.holiday?(Date.new(2014, 11, 4)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 11, 6))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2013, 11, 5))).to eq(false)
+      expect(RiStateHolidays.holiday?(Date.new(2014, 11, 4))).to eq(true)
     end
 
     it "knows Thanksgiving" do
-      RiStateHolidays.holiday?(Date.new(2012, 11, 22)).should be_true
-      RiStateHolidays.holiday?(Date.new(2013, 11, 28)).should be_true
-      RiStateHolidays.holiday?(Date.new(2014, 11, 27)).should be_true
+      expect(RiStateHolidays.holiday?(Date.new(2012, 11, 22))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2013, 11, 28))).to eq(true)
+      expect(RiStateHolidays.holiday?(Date.new(2014, 11, 27))).to eq(true)
     end
   end
 
